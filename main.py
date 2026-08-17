6b29a52e908c10b896834aee32e8064303d50dbd
Ignatius Fairlight <lawaierang@gmail.com>
2026-08-18 07:40:13 +0800

    Allow custom user inputs/prompts

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import argparse
 import os
 
 from dotenv import load_dotenv
@@ -15,12 +16,16 @@ def main() -> None:
         api_key=api_key,
     )
 
+    parser = argparse.ArgumentParser(description="Chatbot")
+    parser.add_argument("user_prompt", type=str, help="User prompt")
+    args = parser.parse_args()
+
     response = client.chat.completions.create(
         model="openrouter/free",
         messages=[
             {
                 "role": "user",
-                "content": "Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.",
+                "content": args.user_prompt,
             }
         ],
     )
